5df1b44eeb2a735ff61a3b783bc2051663199e2a
sliftist <sliftist@gmail.com>
2020-04-17 12:27:53 -0400

    Initial commit.

---
+++ test.js
@@ -0,0 +1,23 @@
+async function test() {
+
+}
+
+test();
+
+var x;
+
+class Test {
+    test() {}
+}
+
+var k = {
+    y: function() {
+
+    }
+};
+
+var z = k.y;
+
+var y = new Test().test;
+
+process.env;
